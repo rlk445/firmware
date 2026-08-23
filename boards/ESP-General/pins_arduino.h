846367cf83aa4a18892f3e6aeca9b4d622177adf
rlk445 <shoenshingh@gmail.com>
2026-08-23 18:06:44 -0300

    Define ILI9341 driver and TFT dimensions
    Added definitions for ILI9341 driver and TFT dimensions.

--- boards/ESP-General/pins_arduino.h
+++ boards/ESP-General/pins_arduino.h
@@ -1,12 +1,9 @@
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
-
 #include <stdint.h>
 #include "soc/soc_caps.h"
-
 #define USB_VID 0x303a
 #define USB_PID 0x1001
-
 // Some boards have too low voltage on this pin (board design bug)
 // Use different pin with 3V and connect with 48
 // and change this setup for the chosen pin (for example 38)
@@ -18,18 +15,14 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
 // RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
 #define RGB_BUILTIN    LED_BUILTIN
 #define RGB_BRIGHTNESS 64
-
 static const uint8_t TX = 43;
 static const uint8_t RX = 44;
-
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
-
 static const uint8_t SS = 10;
 static const uint8_t MOSI = 11;
 static const uint8_t MISO = 13;
 static const uint8_t SCK = 12;
-
 static const uint8_t A0 = 1;
 static const uint8_t A1 = 2;
 static const uint8_t A2 = 3;
@@ -50,7 +43,6 @@ static const uint8_t A16 = 17;
 static const uint8_t A17 = 18;
 static const uint8_t A18 = 19;
 static const uint8_t A19 = 20;
-
 static const uint8_t T1 = 1;
 static const uint8_t T2 = 2;
 static const uint8_t T3 = 3;
@@ -65,5 +57,9 @@ static const uint8_t T11 = 11;
 static const uint8_t T12 = 12;
 static const uint8_t T13 = 13;
 static const uint8_t T14 = 14;
+
+#define ILI9341_DRIVER
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
 
 #endif /* Pins_Arduino_h */
